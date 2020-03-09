010d5a873c60446aa31bf01bc0416339b483e2ec
Josue Rodriguez <smexyjosue@gmail.com>
2020-03-09 14:43:06 -0700

    Create post for server
    * Added server.post for /api/users endpoint
    
    * returned status 201, 400, and 500 relevant to user error or success

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const express = require("express");
+const shortid = require("shortid");
 
 const server = express();
+server.use(express.json());
+
+const users = [];
+
+server.post("/api/users", (req, res) => {
+  const info = req.body;
+  if (info.name.length > 1 && info.bio.length > 1) {
+    info.id = shortid.generate();
+    users.push(info);
+    res.status(201).json(info);
+  } else if (info.name.length < 1 || info.bio.length < 1) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
+  } else {
+    res.status(500).json({
+      errorMessage: "There was an error while saving the user to the database"
+    });
+  }
+});
 
 const PORT = 5000;
 server.listen(PORT, (req, res) => {
