0728eb8de92a9ae3d89ec87c1e497bcc7ed00239
Josue Rodriguez <maybejosue@gmail.com>
2021-05-13 15:07:24 -0700

    Creates .post endpoint to create a single user

--- routes/users.js
+++ routes/users.js
@@ -7,10 +7,16 @@ let persons = [];
 
 // Returns array of users
 users.get("/", (req, res) => {
-  return res.status(200).json({ persons: persons });
+  res.status(200).json({ persons: persons });
 });
 
 // Create single user
+users.post("/", (req, res) => {
+  const id = nanoid();
+  const new_person = { id, ...req.body };
+  persons.push(new_person);
+  res.status(201).json({ person: new_person });
+});
 
 // Returns single user with matching id
 
