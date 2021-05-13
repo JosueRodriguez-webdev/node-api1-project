ff8a2472b2d39eeb60977848bdbf94669dd08378
Josue Rodriguez <maybejosue@gmail.com>
2021-05-13 14:52:15 -0700

    Creates .get endpoint to return entire list of persons

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,21 @@
 const express = require("express");
+const { nanoid } = require("nanoid");
 
 const users = express.Router();
+
+let persons = [];
+
+// Returns array of users
+users.get("/", (req, res) => {
+  return res.status(200).json({ persons: persons });
+});
+
+// Create single user
+
+// Returns single user with matching id
+
+// Removes single user with matching id
+
+// Updates user with matching id
 
 module.exports = users;
